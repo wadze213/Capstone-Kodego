defd2c088962a8d17618342b7b9723faa387d208
wadze213 <94960172+wadze213@users.noreply.github.com>
2022-11-13 12:34:35 +0800

    Git initialization

---
+++ danil-subrepo/capstone-components/src/layouts/AuthFormLayout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+
+const AuthFormLayout = () => {
+  return (
+    <Box>
+      
+    </Box>
+  )
+}
+
+export default AuthFormLayout
