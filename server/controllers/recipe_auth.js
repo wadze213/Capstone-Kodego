95eeb72697a1e22c70c41a90a687c49cd4959250
Gu-ren <114053482+Gu-ren@users.noreply.github.com>
2022-11-26 23:32:14 +0800

    Final Push for onclick task

--- server/controllers/recipe_auth.js
+++ server/controllers/recipe_auth.js
@@ -1,145 +1,149 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-
-
-require('dotenv').config()
+require("dotenv").config();
 const db = mysql.createPool({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE,
-    port: process.env.DATABASE_PORT
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.DATABASE_PORT,
 });
 
+exports.addRecipe = (req, res) => {
+  const recipe_id = req.body.recipe_id;
+  const recipe_name = req.body.recipe_name;
+  const category = req.body.category;
+  const recipe_instruction = req.body.recipe_instruction;
 
-exports.addRecipe=(req,res)=>{
-    
-    const recipe_id = req.body.recipe_id;
-    const recipe_name = req.body.recipe_name;
-    const category = req.body.category;
-    const recipe_instruction = req.body.recipe_instruction;
-    
-    db.query("INSERT INTO recipe (recipe_id, recipe_name, category, instructions, cust_id) VALUES(?, ?, ?, ?, 10);",[
-        recipe_id,
-        recipe_name,
-        category,
-        recipe_instruction
-    ],(err,result)=>{
-        console.log(err);
-    })
-    console.log(recipe_id)
-    
-}
+  db.query(
+    "INSERT INTO recipe (recipe_id, recipe_name, category, instructions, cust_id) VALUES(?, ?, ?, ?, 10);",
+    [recipe_id, recipe_name, category, recipe_instruction],
+    (err, result) => {
+      console.log(err);
+    }
+  );
+  console.log(recipe_id);
+};
 
-exports.addIngredient=(req,res)=>{
+exports.addIngredient = (req, res) => {
+  const recipe_name = req.body.recipe_name;
+  const RecName = Object.values(recipe_name);
+  const ingredient_name = req.body.ingredient_name;
+  const unit_id = parseInt(req.body.unit_id);
+  const quantity = parseInt(req.body.quantity);
 
-    const recipe_name = req.body.recipe_name;
-    const RecName = Object.values(recipe_name)
-    const ingredient_name = req.body.ingredient_name;
-    const unit_id = parseInt(req.body.unit_id);
-    const quantity = parseInt(req.body.quantity);
-    
-    db.query("SELECT ingredient_name FROM ingredient WHERE ingredient_name = ?",ingredient_name,async function(err,result){
-        if(!result.length){
-            db.query("INSERT INTO ingredient (ingredient_name) VALUES(?)",ingredient_name,(err,result)=>{
-                if(!err){
-                    db.query("INSERT INTO recipe_ingredient (recipe_id, ingredient_id, unit_id, quantity) VALUES ((SELECT recipe_id FROM recipe WHERE recipe_name = ?), (SELECT ingredient_id FROM ingredient WHERE ingredient_name = ?), ?, ?);",[
-                        RecName,
-                        ingredient_name,
-                        unit_id,
-                        quantity
-                    ],(err,result)=>{
-                        console.log(err);
-                    })
-                }else{
-                    console.log(err)
+  db.query(
+    "SELECT ingredient_name FROM ingredient WHERE ingredient_name = ?",
+    ingredient_name,
+    async function (err, result) {
+      if (!result.length) {
+        db.query(
+          "INSERT INTO ingredient (ingredient_name) VALUES(?)",
+          ingredient_name,
+          (err, result) => {
+            if (!err) {
+              db.query(
+                "INSERT INTO recipe_ingredient (recipe_id, ingredient_id, unit_id, quantity) VALUES ((SELECT recipe_id FROM recipe WHERE recipe_name = ?), (SELECT ingredient_id FROM ingredient WHERE ingredient_name = ?), ?, ?);",
+                [RecName, ingredient_name, unit_id, quantity],
+                (err, result) => {
+                  console.log(err);
                 }
-            })
-        }else{
-            db.query("INSERT INTO recipe_ingredient (recipe_id, ingredient_id, unit_id, quantity) VALUES ((SELECT recipe_id FROM recipe WHERE recipe_name = ?), (SELECT ingredient_id FROM ingredient WHERE ingredient_name = ?), ?, ?);",[
-                RecName,
-                ingredient_name,
-                unit_id,
-                quantity
-            ],(err,result)=>{
-                console.log(err);
-            })        
+              );
+            } else {
+              console.log(err);
             }
-         })
-
-}
-
-exports.getIngredient=(req,res)=>{
-    const Rec_Name = req.query.Rec_Name;
-    db.query("SELECT i.ingredient_name, u.unit_name,ri.quantity FROM recipe_ingredient ri JOIN ingredient i ON i.ingredient_id = ri.ingredient_id JOIN unit u ON u.unit_id = ri.unit_id WHERE recipe_id = (SELECT recipe_id FROM recipe WHERE recipe_name = ?)",Rec_Name,(err,result)=>{
-        if(err){
-            console.log(err)
-        }else{
-            res.send(result)
-        }
-    })
-    
-}
-
-exports.delIngredient=(req,res)=>{
-    const ingredient_name = req.params.ingredient_name;
-    console.log(ingredient_name)
-    db.query("DELETE FROM recipe_ingredient WHERE ingredient_id = (SELECT ingredient_id FROM ingredient WHERE ingredient_name = ?)",ingredient_name,(err,result)=>{
-            console.log(err)
-    })
-}
-
-exports.cancelRecipe=(req,res)=>{
-    const recipe_name = req.params.recipe_name;
-    console.log(recipe_name)
-    db.query("DELETE FROM recipe_ingredient WHERE recipe_id = (SELECT recipe_id FROM recipe WHERE recipe_name = ?)",recipe_name,(err,result)=>{
-            if(!err){
-                db.query("DELETE FROM recipe WHERE recipe_name = ?",recipe_name,(err,result)=>{
-                    console.log(err)
-                })
-            }else{
-                console.log(err)
-            }
-    })
-}
-
-
-// Glenn
-
-exports.displayRecipe=(req,res)=>{
-
-    
-    
-    db.query("SELECT recipe_id, recipe_name, category,instructions, username FROM recipe INNER JOIN customers  ON recipe.cust_id= customers.cust_id",(err,result)=>{
-        res.send(result)
-       
-    })
-    
-}
-
-exports.displayRecipe1=(req,res)=>{
-
-    const recipe_id = req.params.id;
-    
-    
-        
-        db.query(`SELECT recipe_id, recipe_name, category,instructions, username FROM recipe INNER JOIN customers  ON recipe.cust_id= customers.cust_id WHERE recipe_id = ${recipe_id}`,(err,result)=>{
-            res.send(result)
-          
-        })
-        
+          }
+        );
+      } else {
+        db.query(
+          "INSERT INTO recipe_ingredient (recipe_id, ingredient_id, unit_id, quantity) VALUES ((SELECT recipe_id FROM recipe WHERE recipe_name = ?), (SELECT ingredient_id FROM ingredient WHERE ingredient_name = ?), ?, ?);",
+          [RecName, ingredient_name, unit_id, quantity],
+          (err, result) => {
+            console.log(err);
+          }
+        );
+      }
     }
-    exports.ingredient=(req,res)=>{
-    
-        const ingredient_id = req.params.id;
-    
-        
-        db.query(`SELECT  ri.quantity , mu.unit_name  , i.ingredient_name  FROM recipe_ingredient ri JOIN unit mu on mu.unit_id= ri.unit_id JOIN ingredient i on i.ingredient_id = ri.ingredient_id WHERE recipe_id = ${ingredient_id}`
-        ,(err,result)=>{
-            res.send(result)
-         
-        })
-        
-    }
-    
+  );
+};
 
+exports.getIngredient = (req, res) => {
+  const Rec_Name = req.query.Rec_Name;
+  db.query(
+    "SELECT i.ingredient_name, u.unit_name,ri.quantity FROM recipe_ingredient ri JOIN ingredient i ON i.ingredient_id = ri.ingredient_id JOIN unit u ON u.unit_id = ri.unit_id WHERE recipe_id = (SELECT recipe_id FROM recipe WHERE recipe_name = ?)",
+    Rec_Name,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+};
+
+exports.delIngredient = (req, res) => {
+  const ingredient_name = req.params.ingredient_name;
+  console.log(ingredient_name);
+  db.query(
+    "DELETE FROM recipe_ingredient WHERE ingredient_id = (SELECT ingredient_id FROM ingredient WHERE ingredient_name = ?)",
+    ingredient_name,
+    (err, result) => {
+      console.log(err);
+    }
+  );
+};
+
+exports.cancelRecipe = (req, res) => {
+  const recipe_name = req.params.recipe_name;
+  console.log(recipe_name);
+  db.query(
+    "DELETE FROM recipe_ingredient WHERE recipe_id = (SELECT recipe_id FROM recipe WHERE recipe_name = ?)",
+    recipe_name,
+    (err, result) => {
+      if (!err) {
+        db.query(
+          "DELETE FROM recipe WHERE recipe_name = ?",
+          recipe_name,
+          (err, result) => {
+            console.log(err);
+          }
+        );
+      } else {
+        console.log(err);
+      }
+    }
+  );
+};
+
+// Glenn work
+
+exports.displayRecipe = (req, res) => {
+  db.query(
+    "SELECT recipe_id, recipe_name, category,instructions, username FROM recipe INNER JOIN customers  ON recipe.cust_id= customers.cust_id",
+    (err, result) => {
+      res.send(result);
+    }
+  );
+};
+
+exports.displayRecipe1 = (req, res) => {
+  const recipe_id = req.params.id;
+
+  db.query(
+    `SELECT recipe_id, recipe_name, category,instructions, username FROM recipe INNER JOIN customers  ON recipe.cust_id= customers.cust_id WHERE recipe_id = ${recipe_id}`,
+    (err, result) => {
+      res.send(result);
+    }
+  );
+};
+exports.ingredient = (req, res) => {
+  const ingredient_id = req.params.id;
+
+  db.query(
+    `SELECT  ri.quantity , mu.unit_name  , i.ingredient_name  FROM recipe_ingredient ri JOIN unit mu on mu.unit_id= ri.unit_id JOIN ingredient i on i.ingredient_id = ri.ingredient_id WHERE recipe_id = ${ingredient_id}`,
+    (err, result) => {
+      res.send(result);
+    }
+  );
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const recipe_controller = require('../controllers/recipe_auth');
-const user_controller = require('../controllers/user_auth');
+const recipe_controller = require("../controllers/recipe_auth");
+const user_controller = require("../controllers/user_auth");
 
 router.post("/insertIngredient", recipe_controller.addIngredient);
 router.post("/insertRecipe", recipe_controller.addRecipe);
 router.get("/getIngredient", recipe_controller.getIngredient);
-router.delete("/delIngredient/:ingredient_name", recipe_controller.delIngredient);
+router.delete(
+  "/delIngredient/:ingredient_name",
+  recipe_controller.delIngredient
+);
 router.delete("/cancelRecipe/:recipe_name", recipe_controller.cancelRecipe);
 
 router.post("/registeruser", user_controller.registerUser);
 router.post("/loginuser", user_controller.loginUser);
 router.get("/loginuser", user_controller.loginStatus);
 
-
-
 router.get("/displayRecipe", recipe_controller.displayRecipe);
 router.get("/displayRecipe1/:id", recipe_controller.displayRecipe1);
 router.get("/ingredient/:id", recipe_controller.ingredient);
 
-
-module.exports = router
+module.exports = router;
