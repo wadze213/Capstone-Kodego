63e01abb95b397c3009d384db969f5a3ac773c0c
wadze213 <94960172+wadze213@users.noreply.github.com>
2022-11-27 14:38:46 +0800

    Added missing addIngredient Route

--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -6,6 +6,7 @@ import CreateRecipe from './CreateRecipe';
 import LogIn from './LogIn';
 import Register from './Register';
 import Recipe from './Recipe';
+import RecipeIngredientsForm from '../components/RecipeIngredientsForm';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 
 const Index = () => {
@@ -20,6 +21,7 @@ const Index = () => {
             <Route exact path='/usermenu' element={<UserMenu/>}></Route>
             <Route exact path='/createrecipe' element={<CreateRecipe/>}></Route>
             <Route exact path='/recipe/:id' element={<Recipe/>}></Route>
+            <Route exact path='/createrecipe/addingredient:recipe_name' element={<RecipeIngredientsForm/>}></Route>
         </Routes>
       </Router>
     </div>
